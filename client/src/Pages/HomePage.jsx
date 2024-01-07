452db7d916a279b1c337905139f30c35df6221c3
ANKUR KUMAR <96306426+ankur-357@users.noreply.github.com>
2024-01-07 20:49:39 +0530

    Update HomePage.jsx

--- client/src/Pages/HomePage.jsx
+++ client/src/Pages/HomePage.jsx
@@ -6,7 +6,7 @@ const HomePage = () => {
   const [posts, setPosts] = useState([]);
   useEffect(() => {
     async function getPost() {
-      const post = await axios.get("http://localhost:4000/post", {
+      const post = await axios.get("https://react-backend-new.onrender.com/post", {
         headers: {
           "Access-Control-Allow-Origin": "*",
         },
