7bcdda3ab82f3d065ebbad977e6ae916208546d2
ANKUR KUMAR <96306426+ankur-357@users.noreply.github.com>
2024-01-07 20:50:14 +0530

    Update Login.jsx

--- client/src/Pages/Login.jsx
+++ client/src/Pages/Login.jsx
@@ -18,7 +18,7 @@ const Login = () => {
       password,
     };
 
-    const res = await fetch("http://localhost:4000/login", {
+    const res = await fetch("https://react-backend-new.onrender.com/login", {
       method: "POST",
       body: JSON.stringify(data),
       headers: {
