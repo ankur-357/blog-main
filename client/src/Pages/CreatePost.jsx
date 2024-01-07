50b9d6a59e9fa86bef607323b63e92c3dca6b7c0
ANKUR KUMAR <96306426+ankur-357@users.noreply.github.com>
2024-01-07 20:47:45 +0530

    Update CreatePost.jsx

--- client/src/Pages/CreatePost.jsx
+++ client/src/Pages/CreatePost.jsx
@@ -17,7 +17,7 @@ const CreatePost = () => {
     formData.append("content", content);
     formData.append("image", files);
 
-    await axios.post("http://localhost:4000/post", formData, {
+    await axios.post("https://react-backend-new.onrender.com/post", formData, {
       withCredentials: true,
       headers: {
         "Content-Type": "multipart/form-data",
