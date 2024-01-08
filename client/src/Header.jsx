33725d09241c5151f6a0bbf3507917f3ee89dfe1
ANKUR KUMAR <96306426+ankur-357@users.noreply.github.com>
2024-01-08 08:17:42 +0530

    Update Header.jsx

--- client/src/Header.jsx
+++ client/src/Header.jsx
@@ -5,7 +5,7 @@ import { UserContext } from "./UserContext";
 const Header = () => {
   const { setUserInfo, userInfo } = useContext(UserContext);
   useEffect(() => {
-    fetch("http://localhost:4000/profile", {
+    fetch("https://react-backend-new.onrender.com//profile", {
       credentials: "include",
     }).then((res) => {
       res.json().then((userinfo) => setUserInfo(userinfo));
