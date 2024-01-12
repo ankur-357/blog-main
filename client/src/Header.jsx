7895abf37ab8dbc158131e68ac919b5db2437e23
ANKUR KUMAR <96306426+ankur-357@users.noreply.github.com>
2024-01-12 11:13:02 +0530

    Update Header.jsx

--- client/src/Header.jsx
+++ client/src/Header.jsx
@@ -5,7 +5,7 @@ import { UserContext } from "./UserContext";
 const Header = () => {
   const { setUserInfo, userInfo } = useContext(UserContext);
   useEffect(() => {
-    fetch("https://react-backend-new.onrender.com/profile", {
+    fetch("https://blog-main-hxzfljnsf-ankur-357.vercel.app/profile", {
       credentials: "include",
     }).then((res) => {
       res.json().then((userinfo) => setUserInfo(userinfo));
@@ -13,7 +13,7 @@ const Header = () => {
   }, []);
   const handleLogout = (e) => {
     e.preventDefault();
-    fetch("https://react-backend-new.onrender.com/logout", {
+    fetch("https://blog-main-hxzfljnsf-ankur-357.vercel.app/logout", {
       method: "POST",
       credentials: "include",
       headers: {
